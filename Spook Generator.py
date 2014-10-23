55e46d64e2a34444c7cb85df972967297865894f
Daedalus191 <bfentonq@hotmail.com>
2014-10-23 19:37:19 +0100

    Spook Generator
    This generates spook.

---
+++ Spook Generator.py
@@ -0,0 +1,9 @@
+
+#Input your level of spook
+x=int(input('Please input the level of spookyness: '))
+
+spook= '0' * x
+
+#This will print your level of spook
+for z in range (x):
+    print(str(x)+'Sp'+spook+'ky4me')
